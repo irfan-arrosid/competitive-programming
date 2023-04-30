0d7d1e8142300befd448c5317fab48bb791e2a43
irfan-arrosid <irfanarrosid4@gmail.com>
2023-04-30 23:41:51 +0700

    Solving SimpleArraySum on HackerRank using JavaScript

---
+++ algorithms/simple-array-sum.js
@@ -0,0 +1,14 @@
+let array = [1, 2, 3, 4],
+    sum = 0
+
+    // array.forEach(sumArray)
+    
+    // function sumArray(arr) {
+    //     sum += arr
+    // }
+    
+    array.forEach(arr => {
+        sum += arr
+    })
+
+console.log(sum);
